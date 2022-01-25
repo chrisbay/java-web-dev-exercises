e32804bb42e2222ab5d95988b04cfe7034160f12
Chris Bay <chris@chrisbay.net>
2022-01-25 12:44:35 -0600

    Demos some Java features

--- src/org/launchcode/java/demos/Hello.java
+++ src/org/launchcode/java/demos/Hello.java
@@ -5,6 +5,29 @@ package org.launchcode.java.demos;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+
+        // primitive data types
+        int year = 2022;
+        double pi = 3.14;
+        boolean isTuesday = true;
+        char myChar = 'c';
+
+        // object data types
+        String name = "Chris Bay";
+        Integer nextYear = 2023;
+        Double longerPi = 3.14159;
+        Boolean isWednesday = false;
+        Character anotherChar = 'b';
+
+        // arrays in Java have FIXED LENGTH
+        String[] names = {"Chris", "Annie", "Jack"};
+        names[0] = "Christopher";
+        int[] years = new int[10];
+
+        boolean isEqual = (year == 2000);
+
+        // nextYear == 2000;
+        nextYear.equals(2000);
+
     }
 }
